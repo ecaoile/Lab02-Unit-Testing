bd26a87ad0bc2ab8939b80847a00ead46a873747
ecaoile <unfie555@gmail.com>
2018-06-07 15:33:43 -0700

    put main in try/catch block

--- Lab02_Unit_Testing/Lab02_Unit_Testing/Program.cs
+++ Lab02_Unit_Testing/Lab02_Unit_Testing/Program.cs
@@ -10,49 +10,51 @@ namespace Lab02_Unit_Testing
             Console.WriteLine("Welcome to Lab 02 ATM!");
             bool inService = true;
             decimal myBalance = 555.55m;
-            while (inService == true)
+            try
             {
-                // main menu that appears at the beginning of every loop
-                Console.WriteLine("How can we help you today?");
-                Console.WriteLine("1. View Balance");
-                Console.WriteLine("2. Withdraw Money");
-                Console.WriteLine("3. Add Money");
-                Console.WriteLine("4. Exit");
-
-                string userChoice = Console.ReadLine();
-
-                // loops until user chooses a valid menu option
-                while (userChoice != "1" && userChoice != "2" && userChoice != "3" && userChoice != "4")
+                while (inService == true)
                 {
-                    Console.WriteLine("You did not choose a valid option. Please choose a number between 1 and 4");
-                    userChoice = Console.ReadLine();
-                }
+                    // main menu that appears at the beginning of every loop
+                    Console.WriteLine("How can we help you today?");
+                    Console.WriteLine("1. View Balance");
+                    Console.WriteLine("2. Withdraw Money");
+                    Console.WriteLine("3. Add Money");
+                    Console.WriteLine("4. Exit");
 
-                // switch case to handle the different ATM operations
-                switch (userChoice)
-                {
-                    case "1":
-                        Console.WriteLine($"Your current balance is {myBalance:c}.");
-                        Console.WriteLine("Returning to main menu.\n");
-                        break;
-                    case "2":
-                        Console.WriteLine("\nHow much money would you like to withdraw?");
-                        try
-                        {
-                            decimal myWithdrawl = Convert.ToDecimal(Console.ReadLine());
+                    string userChoice = Console.ReadLine();
 
-                            if (myWithdrawl > myBalance)
-                                Console.WriteLine("You don't have that much money!\n");
-                            else if (myWithdrawl < 0)
-                                Console.WriteLine("You can't withdraw negative amounts!");
+                    // loops until user chooses a valid menu option
+                    while (userChoice != "1" && userChoice != "2" && userChoice != "3" && userChoice != "4")
+                    {
+                        Console.WriteLine("You did not choose a valid option. Please choose a number between 1 and 4");
+                        userChoice = Console.ReadLine();
+                    }
 
-                            else
+                    // switch case to handle the different ATM operations
+                    switch (userChoice)
+                    {
+                        case "1":
+                            Console.WriteLine($"Your current balance is {myBalance:c}.");
+                            Console.WriteLine("Returning to main menu.\n");
+                            break;
+                        case "2":
+                            Console.WriteLine("\nHow much money would you like to withdraw?");
+                            try
                             {
-                                myBalance = Withdraw(myBalance, myWithdrawl);
-                                Console.WriteLine($"Successfully withdrew {myWithdrawl:c}.");
-                                Console.WriteLine($"Your current balance is {myBalance:c}.\n");
+                                decimal myWithdrawl = Convert.ToDecimal(Console.ReadLine());
+
+                                if (myWithdrawl > myBalance)
+                                    Console.WriteLine("You don't have that much money!\n");
+                                else if (myWithdrawl < 0)
+                                    Console.WriteLine("You can't withdraw negative amounts!");
+
+                                else
+                                {
+                                    myBalance = Withdraw(myBalance, myWithdrawl);
+                                    Console.WriteLine($"Successfully withdrew {myWithdrawl:c}.");
+                                    Console.WriteLine($"Your current balance is {myBalance:c}.\n");
+                                }
                             }
-                        }
                         catch(FormatException)
                         {
                             Console.WriteLine("Error: you didn't enter a valid number!");
@@ -69,47 +71,56 @@ namespace Lab02_Unit_Testing
                             Console.WriteLine("Returning to main menu.\n");
                         }
                         break;
-                    case "3":
-                        Console.WriteLine("\nHow much money would you like to deposit?");
-                        try
-                        {
-                            decimal myDeposit = Convert.ToDecimal(Console.ReadLine());
-                            if (myDeposit < 0)
-                                Console.WriteLine("You can't withdraw negative amounts!");
-                            else
+                        case "3":
+                            Console.WriteLine("\nHow much money would you like to deposit?");
+                            try
                             {
-                                myBalance = Deposit(myBalance, myDeposit);
-                                Console.WriteLine($"Thank you for your deposit of {myDeposit:c}.");
-                                Console.WriteLine($"Your balance is now {myBalance:c}.\n");
+                                decimal myDeposit = Convert.ToDecimal(Console.ReadLine());
+                                if (myDeposit < 0)
+                                    Console.WriteLine("You can't withdraw negative amounts!");
+                                else
+                                {
+                                    myBalance = Deposit(myBalance, myDeposit);
+                                    Console.WriteLine($"Thank you for your deposit of {myDeposit:c}.");
+                                    Console.WriteLine($"Your balance is now {myBalance:c}.\n");
+                                }
                             }
-                        }
-                        catch (FormatException)
-                        {
-                            Console.WriteLine("Error: you didn't enter a valid number!");
-                            Console.WriteLine("Deposit failed.");
-                        }
-                        catch (Exception e)
-                        {
-                            Console.WriteLine(e.Message);
-                            Console.WriteLine("Deposit failed.");
-                            throw;
-                        }
-                        finally
-                        {
-                            Console.WriteLine("Returning to main menu.\n");
-                        }
-                        break;
-                    case "4":
-                        inService = false;
-                        break;
-                    default:
-                        break;
+                            catch (FormatException)
+                            {
+                                Console.WriteLine("Error: you didn't enter a valid number!");
+                                Console.WriteLine("Deposit failed.");
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e.Message);
+                                Console.WriteLine("Deposit failed.");
+                                throw;
+                            }
+                            finally
+                            {
+                                Console.WriteLine("Returning to main menu.\n");
+                            }
+                            break;
+                        case "4":
+                            inService = false;
+                            break;
+                        default:
+                            break;
                 }
             }
+        }
 
-            // closing message before exit
-            Console.WriteLine("\nThank you for using Lab 02 ATM. Have a nice day!");
-            Console.ReadLine();
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            finally
+            {
+                // closing message before exit
+                Console.WriteLine("\nThank you for using Lab 02 ATM. Have a nice day!");
+                Console.ReadLine();
+            }
         }
 
         /// <summary>
